3a811ceba6b340ebf401da51452ffc2fea939d34
gzgr <gzgr@h4c0.com>
2023-10-15 18:19:13 +0900

    fix

--- update.py
+++ update.py
@@ -1,13 +1,6 @@
 import requests
 import yaml
 
-def get_content_from_url(url):
-    # URL에서 데이터 가져오기
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise ValueError(f"Failed to get data from the URL. Status code: {response.status_code}")
-    return response.text
-
 def get_version_from_url(url):
     # URL에서 데이터 가져오기
     response = requests.get(url)
@@ -17,14 +10,18 @@ def get_version_from_url(url):
     # YAML 데이터 파싱하기
     data = yaml.safe_load(response.text)
 
-    # version 정보 반환하기
-    return data.get('arasaka', {}).get('version')
+    # data 양식
+    #   - arasaka:
+    #     - version: 1.0
+    #     - release_date: 2023-10-15
+    
+    # 버전 정보 가져오기
+    version_info = data[0]["arasaka"][0]["version"]
+    return version_info
 
 if __name__ == "__main__":
     url = "https://raw.githubusercontent.com/gzgr/arasaka/main/version.yml"
-    content = get_content_from_url(url)
-    print(content)
-    
+
     version_info = get_version_from_url(url)
     if version_info:
         print(f"Version: {version_info}")
